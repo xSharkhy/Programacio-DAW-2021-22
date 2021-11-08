2a8341d57a7db30296fb1e54ee22279a7214a0c6
xSharkhy <ismbla01@alu.iesserpis.org>
2021-11-08 19:17:58 +0100

    made another problem

--- src/javaCourse/chattyBot/booleanExpression.java
+++ src/javaCourse/chattyBot/booleanExpression.java
@@ -2,17 +2,15 @@ package javaCourse.chattyBot;
 
 public class booleanExpression {
     public static void main(String[] args) {
-        boolean b1 = false;
-        boolean b2 = true;
-        boolean b3 = b1 ^ b2;
-        int a = ((12241 % 100) / 10) % 10;;
-        System.out.println(a);
-        char c = '1' + '3';
-        System.out.println(c);
-        System.out.println(-30 / 7);
-        char[] array = new char[-1];
-        char[] array2 = { 'a', 'b', 'c', 'd' };
-        char[] array4 = new char[1];
-        char[] array5 = new char[0];
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+            if (i < 3) {
+                continue;
+            } else {
+                for (int j = 0; j < 5; j++) {
+                    System.out.println(j);
+                }
+            }
+        }
     }
 }
